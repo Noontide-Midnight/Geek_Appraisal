bf5a368a16b7e10bd4c3aa82eb57146385442676
Noontide-Midnight <dudugeorgelzh@icloud.com>
2022-10-24 23:55:46 +0800

    test

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+int main(int argc, char *argv[])
+{
+
+	printf("Hello World!test\n");
+
 	return 0;
 }
-

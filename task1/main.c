8f35447e30a9b3e329f7ec0fae3c9bfcf774acde
Noontide-Midnight <dudugeorgelzh@icloud.com>
2022-10-26 22:43:48 +0800

    project

--- task1/main.c
+++ task1/main.c
@@ -3,8 +3,18 @@
 
 int main(int argc, char *argv[])
 {
-
-	printf("Hello World!test\n");
+	double x, y, Y;
+	y = 1;
+	x = 0;
+	Y = 2;
+	printf("x        y\n");
+	while (Y - y >= 0.000001)
+	{
+		Y = y;
+		y *= 0.999;
+		x += 0.001;
+		printf("%f    %f\n", x, y);
+	}
 
 	return 0;
 }
